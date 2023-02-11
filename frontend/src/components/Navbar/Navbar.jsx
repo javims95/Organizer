b7c1e5f8a368d095e159621c9398ba1016b28b66
javims95 <javiermunozsanchez95@gmail.com>
2023-02-11 22:04:25 +0100

    New navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,50 +1,83 @@
-import React, { useRef } from 'react';
-import './Navbar.css'
+import React, { Component } from 'react';
+import './Navbar.scss';
 
-const Navbar = () => {
-    const navbarLinks = [
-        { name: 'Inicio', href: '/' },
-        { name: 'Calendario', href: '/calendario' },
-        { name: 'Tareas', href: '/tareas' },
-        { name: 'Contact', href: '/contacto' },
-        { name: 'Registro', href: '/registro' },
-        { name: 'Mi cuenta', href: '/cuenta' },
-    ];
+export default class Navbar extends Component {
 
-    const navLinks = useRef(null);
-    const links = useRef([]);
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: true,
+            selected: 'Home'
+        };
+    }
 
-    const toggleNav = () => {
-        links.current.forEach(link => {
-            link.classList.toggle("fade");
-        });
-        navLinks.current.classList.toggle("open");
+    toggleMenu = () => {
+        this.setState({ open: !this.state.open });
     };
 
-    return (
-        <nav className='main-nav'>
-            <div className="logo">
-                <img src="/logo.png" alt="Logo Image" />
-            </div>
-            <div className="hamburger" onClick={toggleNav}>
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-            </div>
-            <ul className="nav-links" ref={navLinks}>
-                {navbarLinks.map((link, item) => (
-                    <li key={link.name} ref={el => (links.current[item - 1] = el)}>
-                        <a href={link.href}>{link.name}
-                            <span className="border border-top"></span>
-                            <span className="border border-right"></span>
-                            <span className="border border-bottom"></span>
-                            <span className="border border-left"></span>
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
-};
+    selectOption = (option) => {
+        this.setState({ selected: option, open: false });
+    };
 
-export default Navbar;
+    render() {
+        const { open, selected } = this.state;
+        return (
+            <div className="app">
+                <div className="header">
+                    <h2>{selected}</h2>
+                    <button onClick={this.toggleMenu}>
+                        {open ? 'Close' : 'Open'} Menu
+                    </button>
+                </div>
+                {open && (
+                    <div className="menu">
+                        <div
+                            className={`menu-option ${selected === 'Home' ? 'selected' : ''}`}
+                            onClick={() => this.selectOption('Home')}
+                        >
+                            Home
+                        </div>
+                        <div
+                            className={`menu-option ${selected === 'About' ? 'selected' : ''}`}
+                            onClick={() => this.selectOption('About')}
+                        >
+                            About
+                        </div>
+                        <div
+                            className={`menu-option ${selected === 'Services' ? 'selected' : ''}`}
+                            onClick={() => this.selectOption('Services')}
+                        >
+                            Services
+                        </div>
+                        <div
+                            className={`menu-option ${selected === 'Contact' ? 'selected' : ''}`}
+                            onClick={() => this.selectOption('Contact')}
+                        >
+                            Contact
+                        </div>
+                        <div
+                            className={`menu-option ${selected === 'Settings' ? 'selected' : ''}`}
+                            onClick={() => this.selectOption('Settings')}
+                        >
+                            Settings
+                        </div>
+                        <div className="menu-option has-submenu">
+                            <div
+                                className={`submenu-option ${selected === 'Submenu 1' ? 'selected' : ''}`}
+                                onClick={() => this.selectOption('Submenu 1')}
+                            >
+                                Submenu 1
+                            </div>
+                            <div
+                                className={`submenu-option ${selected === 'Submenu 2' ? 'selected' : ''}`}
+                                onClick={() => this.selectOption('Submenu 2')}
+                            >
+                                Submenu 2
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
