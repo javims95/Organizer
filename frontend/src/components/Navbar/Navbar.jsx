21b165afecc89882bb32f1cbd7248ab11ae73081
javims95 <javiermunozsanchez95@gmail.com>
2023-02-14 16:39:04 +0100

    Functional menu with subitems and animation

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Navbar.scss';
 import Icon from '../icon/icon.jsx';
 import menuItems from './conf/menuItems';
 
+
+
 const Navbar = () => {
     const [open, setOpen] = useState({});
+    const ulRef = useRef(null);
 
     const handleClick = (e, menuItem) => {
         e.preventDefault();
@@ -12,7 +15,30 @@ const Navbar = () => {
             ...open,
             [menuItem]: !open[menuItem],
         });
+        if (open[menuItem]) {
+            setTimeout(() => {
+                ulRef.current.classList.add('closed');
+            }, 500);
+        } else {
+            ulRef.current.classList.remove('closed');
+        }
     };
+
+    useEffect(() => {
+        const handleTransitionEnd = (e) => {
+            if (e.propertyName === 'height' && !open) {
+                setIsClosing(false);
+            }
+        };
+        const ulElement = ulRef.current;
+        if (ulElement) {
+            ulElement.addEventListener('transitionend', handleTransitionEnd);
+            return () => {
+                ulElement.removeEventListener('transitionend', handleTransitionEnd);
+            };
+        }
+    }, [open]);
+
 
     return (
         <div className="page-header">
@@ -27,39 +53,25 @@ const Navbar = () => {
                         <li className={menuItem.subItems ? 'has-dropdown' : ''} key={menuItem.name}>
                             {menuItem.subItems ? (
                                 <a href={menuItem.link} onClick={(e) => handleClick(e, menuItem.name)}>
-                                    <Icon
-                                        icon={menuItem.icon}
-                                        width={"20"}
-                                        height={"20"}
-                                    />
+                                    <Icon icon={menuItem.icon} width="20" height="20" />
                                     <span className="nav-link">{menuItem.name}</span>
-                                    <Icon
-                                        icon={'angle-left'}
-                                        width={"20"}
-                                        height={"20"}
-                                        className={open[menuItem.name] ? 'dropdown down' : 'dropdown'}
-                                    />
+                                    <Icon icon={'angle-left'} width="20" height="20" className={open[menuItem.name] ? 'dropdown down' : 'dropdown'} />
                                 </a>
                             ) : (
-                                <a href={menuItem.link} >
-                                    <Icon
-                                        icon={menuItem.icon}
-                                        width={"20"}
-                                        height={"20"}
-                                    />
-                                    <span className="nav-link">{menuItem.name}</span>                                   
+                                <a href={menuItem.link}>
+                                    <Icon icon={menuItem.icon} width="20" height="20" />
+                                    <span className="nav-link">{menuItem.name}</span>
                                 </a>
                             )}
                             {menuItem.subItems && (
-                                <ul className={open[menuItem.name] ? 'subitem-dropdown open' : 'subitem-dropdown close'} >
+                                <ul
+                                    className={`subitem-dropdown ${open[menuItem.name] ? 'open' : 'closed'}`}
+                                    ref={ulRef}
+                                >
                                     {menuItem.subItems.map((subItem) => (
                                         <li key={subItem.name}>
                                             <a href={subItem.link}>
-                                                <Icon
-                                                    icon={'circle'}
-                                                    width={"20"}
-                                                    height={"20"}
-                                                />
+                                                <Icon icon={'circle'} width="20" height="20" />
                                                 <span className="nav-link">{subItem.name}</span>
                                             </a>
                                         </li>
