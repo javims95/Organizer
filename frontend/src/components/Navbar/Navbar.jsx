9833d574f84f66629f98b471d598cf3f31228d9d
javims95 <javiermunozsanchez95@gmail.com>
2023-02-17 21:30:28 +0100

    Save changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,8 @@ import Search from '../Search/Search.jsx'
 
 const Navbar = () => {
     const [open, setOpen] = useState({});
+    const [menuHidden, setMenuHidden] = useState(false);
+    const [isHovering, setIsHovering] = useState(false);
     const ulRef = useRef(null);
 
     const handleClick = (e, menuItem) => {
@@ -23,6 +25,18 @@ const Navbar = () => {
         }
     };
 
+    const handleHideMenu = () => {
+        setMenuHidden(!menuHidden);
+        if (!menuHidden) {
+            document.body.classList.remove('nav-expanded');
+            document.body.classList.add('nav-collapsed');
+        } else {
+            document.body.classList.remove('nav-collapsed');
+            document.body.classList.add('nav-expanded');
+        }
+    };
+
+
     useEffect(() => {
         const handleTransitionEnd = (e) => {
             if (e.propertyName === 'height' && !open) {
@@ -38,46 +52,71 @@ const Navbar = () => {
         }
     }, [open]);
 
+    const handleMouseEnter = () => {
+        if (document.body.classList.contains('nav-collapsed')) {
+            document.body.classList.remove('nav-collapsed');
+            document.body.classList.add('nav-expanded');
+            setIsHovering(true);
+        }
+    };
+
+    const handleMouseLeave = () => {
+        if (isHovering) {
+            document.body.classList.remove('nav-expanded');
+            document.body.classList.add('nav-collapsed');
+            setIsHovering(false);
+        }
+    };
 
     return (
-        <div className="page-header">
-            <nav className='navbar'>                
-                <Search />
-                <ul className='primary-nav'>
-                    {menuItems.map((menuItem) => (
-                        <li className={menuItem.subItems ? 'has-dropdown' : ''} key={menuItem.name}>
-                            {menuItem.subItems ? (
-                                <a href={menuItem.link} onClick={(e) => handleClick(e, menuItem.name)}>
-                                    <Icon icon={menuItem.icon} width="20" height="20" />
-                                    <span className="nav-link">{menuItem.name}</span>
-                                    <Icon icon={'angle-left'} width="20" height="20" className={open[menuItem.name] ? 'dropdown down' : 'dropdown'} />
-                                </a>
-                            ) : (
-                                <a href={menuItem.link}>
-                                    <Icon icon={menuItem.icon} width="20" height="20" />
-                                    <span className="nav-link">{menuItem.name}</span>
-                                </a>
-                            )}
-                            {menuItem.subItems && (
-                                <ul
-                                    className={`subitem-dropdown ${open[menuItem.name] ? 'open' : 'closed'}`}
-                                    ref={ulRef}
-                                >
-                                    {menuItem.subItems.map((subItem) => (
-                                        <li key={subItem.name}>
-                                            <a href={subItem.link}>
-                                                <Icon icon={'circle'} width="20" height="20" />
-                                                <span className="nav-link">{subItem.name}</span>
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </li>
-                    ))}
-                </ul>
+        <>
+            <div className='page-header' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <nav className='navbar'>
+                    {/* <button class="toggle-mob-menu" aria-expanded="false" aria-label="open menu" onClick={handleHideMenu}>
+                        <Icon icon="angle-left" width="20" height="20" />
+                    </button> */}
+                    <Search />
+                    <ul className='primary-nav'>
+                        {menuItems.map((menuItem) => (
+                            <li className={menuItem.subItems ? 'has-dropdown' : ''} key={menuItem.name}>
+                                {menuItem.subItems ? (
+                                    <a href={menuItem.link} onClick={(e) => handleClick(e, menuItem.name)}>
+                                        <Icon icon={menuItem.icon} width="20" height="20" />
+                                        <span className="nav-link">{menuItem.name}</span>
+                                        <Icon icon={'angle-left'} width="20" height="20" className={open[menuItem.name] ? 'dropdown down' : 'dropdown'} />
+                                    </a>
+                                ) : (
+                                    <a href={menuItem.link}>
+                                        <Icon icon={menuItem.icon} width="20" height="20" />
+                                        <span className="nav-link">{menuItem.name}</span>
+                                    </a>
+                                )}
+                                {menuItem.subItems && (
+                                    <ul
+                                        className={`subitem-dropdown ${open[menuItem.name] ? 'open' : 'closed'}`}
+                                        ref={ulRef}
+                                    >
+                                        {menuItem.subItems.map((subItem) => (
+                                            <li key={subItem.name}>
+                                                <a href={subItem.link}>
+                                                    <Icon icon={'circle'} width="20" height="20" />
+                                                    <span className="nav-link">{subItem.name}</span>
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+            <nav className='navbar-secondary'>
+                <div class="toggle-menu" onClick={handleHideMenu}>
+                    <Icon icon="bars" width="25" height="25" />
+                </div>
             </nav>
-        </div>
+        </>
     );
 };
 
