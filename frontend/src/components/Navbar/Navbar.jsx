d8998f79aa4af708fa57c610a6095af3c0429c19
javims95 <javiermunozsanchez95@gmail.com>
2023-02-18 10:47:14 +0100

    Secondary nav, new icons, styles

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,12 @@ const Navbar = () => {
     const [open, setOpen] = useState({});
     const [menuHidden, setMenuHidden] = useState(false);
     const [isHovering, setIsHovering] = useState(false);
+    const [isFullScreen, setIsFullScreen] = useState(false);
     const ulRef = useRef(null);
+
+    const handleFullScreenChange = () => {
+        setIsFullScreen(document.fullscreenElement !== null);
+    };
 
     const handleClick = (e, menuItem) => {
         e.preventDefault();
@@ -52,6 +57,14 @@ const Navbar = () => {
         }
     }, [open]);
 
+    useEffect(() => {
+        document.addEventListener('fullscreenchange', handleFullScreenChange);
+
+        return () => {
+            document.removeEventListener('fullscreenchange', handleFullScreenChange);
+        };
+    }, []);
+
     const handleMouseEnter = () => {
         if (document.body.classList.contains('nav-collapsed')) {
             document.body.classList.remove('nav-collapsed');
@@ -68,53 +81,80 @@ const Navbar = () => {
         }
     };
 
+    const handleFullScreen = () => {
+        if (document.fullscreenEnabled) {
+            if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen();
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                }
+            }
+        }
+    };
+
     return (
         <>
-            <div className='page-header' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <nav className='navbar'>
-                    {/* <button class="toggle-mob-menu" aria-expanded="false" aria-label="open menu" onClick={handleHideMenu}>
-                        <Icon icon="angle-left" width="20" height="20" />
-                    </button> */}
-                    <Search />
-                    <ul className='primary-nav'>
-                        {menuItems.map((menuItem) => (
-                            <li className={menuItem.subItems ? 'has-dropdown' : ''} key={menuItem.name}>
-                                {menuItem.subItems ? (
-                                    <a href={menuItem.link} onClick={(e) => handleClick(e, menuItem.name)}>
-                                        <Icon icon={menuItem.icon} width="20" height="20" />
-                                        <span className="nav-link">{menuItem.name}</span>
-                                        <Icon icon={'angle-left'} width="20" height="20" className={open[menuItem.name] ? 'dropdown down' : 'dropdown'} />
-                                    </a>
-                                ) : (
-                                    <a href={menuItem.link}>
-                                        <Icon icon={menuItem.icon} width="20" height="20" />
-                                        <span className="nav-link">{menuItem.name}</span>
-                                    </a>
-                                )}
-                                {menuItem.subItems && (
-                                    <ul
-                                        className={`subitem-dropdown ${open[menuItem.name] ? 'open' : 'closed'}`}
-                                        ref={ulRef}
-                                    >
-                                        {menuItem.subItems.map((subItem) => (
-                                            <li key={subItem.name}>
-                                                <a href={subItem.link}>
-                                                    <Icon icon={'circle'} width="20" height="20" />
-                                                    <span className="nav-link">{subItem.name}</span>
-                                                </a>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-            </div>
+            <nav className='navbar-primary' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <Search />
+                <ul className='primary-nav'>
+                    {menuItems.map((menuItem) => (
+                        <li className={menuItem.subItems ? 'has-dropdown' : ''} key={menuItem.name}>
+                            {menuItem.subItems ? (
+                                <a href={menuItem.link} onClick={(e) => handleClick(e, menuItem.name)}>
+                                    <Icon icon={menuItem.icon} width="20" height="20" />
+                                    <span className="nav-link">{menuItem.name}</span>
+                                    <Icon icon={'angle-left'} width="20" height="20" className={open[menuItem.name] ? 'dropdown down' : 'dropdown'} />
+                                </a>
+                            ) : (
+                                <a href={menuItem.link}>
+                                    <Icon icon={menuItem.icon} width="20" height="20" />
+                                    <span className="nav-link">{menuItem.name}</span>
+                                </a>
+                            )}
+                            {menuItem.subItems && (
+                                <ul
+                                    className={`subitem-dropdown ${open[menuItem.name] ? 'open' : 'closed'}`}
+                                    ref={ulRef}
+                                >
+                                    {menuItem.subItems.map((subItem) => (
+                                        <li key={subItem.name}>
+                                            <a href={subItem.link}>
+                                                <Icon icon={'circle'} width="20" height="20" />
+                                                <span className="nav-link">{subItem.name}</span>
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </nav>
             <nav className='navbar-secondary'>
-                <div class="toggle-menu" onClick={handleHideMenu}>
-                    <Icon icon="bars" width="25" height="25" />
-                </div>
+                <ul className='nav-list-left'>
+                    <li>
+                        <a className='toggle-menu nav-link' href="#" onClick={handleHideMenu}>
+                            <Icon icon="bars" width="20" height="20" className={'icon-menu'} />
+                        </a>
+                    </li>
+                    <li>
+                        <a className='nav-link' href="">Inicio</a>
+                    </li>
+                </ul>
+                <ul className='nav-list-notification'>
+                    <li>
+                        <a className='nav-link' href="#">
+                            <Icon icon="bell" width="20" height="20" className={'icon-menu'} />
+                            <span className='notification-counter'>12</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a className='nav-link' href="#" onClick={handleFullScreen}>
+                            <Icon icon={isFullScreen ? 'compress-arrows' : 'expand-arrows'} width="20" height="20" className={'icon-menu'} />
+                        </a>
+                    </li>
+                </ul>
             </nav>
         </>
     );
